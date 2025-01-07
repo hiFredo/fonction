a2a490a7341166168e58abbc1988d6023ee0db80
Mazino <fredozv01@gmail.com>
2025-01-07 19:22:46 +0100

    fix erreur

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-def recupere_et_afficher_info_personne(numero_personne): 
- nom , age = recupere_info_personne()
- afficher_info_personne(numero_personne,nom,age)
+def majeur(age):
+    if age <=0 :
+        return False
+    if age >= 18:
+        return True
+
 
 def recupere_info_personne(numero_personne):
     nom_personne = input(f"Nom de la personne {numero_personne} : ")
     age_personne = input(f"Age de la personne {numero_personne} :  ")
-    return nom_personne,age_personne
+    return nom_personne, int(age_personne)
     
 
-def afficher_info_personne(numero_personne,nom,age):
+def afficher_info_personne(numero_personne,nom,age :int):
     print(f"La personne {numero_personne} est {nom} et son age est {age} ans  ")
     print (f"Le nom comporte  {len(nom) } de lettre ")
+
+      #  vrai majeur et faut mineur
+    if majeur(age) :
+        print("Il est majeur")
+    else:
+       print("Il est mineur")
+
+def recupere_et_afficher_info_personne(numero_personne): 
+ nom , age = recupere_info_personne(numero_personne)
+ afficher_info_personne(numero_personne,nom,age)
 
 
 nb_personne = 3 
